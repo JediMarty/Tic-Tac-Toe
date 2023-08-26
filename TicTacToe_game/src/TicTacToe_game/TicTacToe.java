2fcd566d891dbed78bfb553ad5c2c7cf399f6d92
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-08-26 18:21:44 +0300

    Update TicTacToe.java

--- TicTacToe_game/src/TicTacToe_game/TicTacToe.java
+++ TicTacToe_game/src/TicTacToe_game/TicTacToe.java
@@ -4,10 +4,15 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		char[] [] BOARD = {{' ','|',' ','|',' '},
-				   {'-','+','-','+','-'},
+				           {'-','+','-','+','-'},
 		                   {' ','|',' ','|',' '},
 		                   {'-','+','-','+','-'},
 		                   {' ','|',' ','|',' '}};
+		
+		theBOARD(BOARD);
+		}
+	
+	public static void theBOARD(char[][] BOARD) {
 		for (char[] row : BOARD) {
 			for (char coll : row) {
 				System.out.print(coll);
