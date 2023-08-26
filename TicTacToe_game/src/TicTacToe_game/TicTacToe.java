e149aad8ed225c8561a27b2c0b15dc2eca7d715c
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-08-26 19:00:19 +0300

    Update TicTacToe.java

--- TicTacToe_game/src/TicTacToe_game/TicTacToe.java
+++ TicTacToe_game/src/TicTacToe_game/TicTacToe.java
@@ -1,15 +1,29 @@
 package TicTacToe_game;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String start;
 		char[] [] BOARD = {{' ','|',' ','|',' '},
-				           {'-','+','-','+','-'},
+			           {'-','+','-','+','-'},
 		                   {' ','|',' ','|',' '},
 		                   {'-','+','-','+','-'},
 		                   {' ','|',' ','|',' '}};
 		
+		System.out.println("Welcome to Tic-Tac-Toe");
+		System.out.println("Let's play!");
+		do {
+			System.out.println("Press Y");
+			start = scanner.nextLine().toUpperCase();
+			
+		}while(!start.equals("Y"));
+			
 		theBOARD(BOARD);
+			
+		
 		}
 	
 	public static void theBOARD(char[][] BOARD) {
