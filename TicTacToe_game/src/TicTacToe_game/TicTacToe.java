52b2af16541077e510534984142c87a0eb869df2
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-08-28 19:09:19 +0300

    Update TicTacToe.java

--- TicTacToe_game/src/TicTacToe_game/TicTacToe.java
+++ TicTacToe_game/src/TicTacToe_game/TicTacToe.java
@@ -8,6 +8,7 @@ public class TicTacToe {
 		Scanner scanner = new Scanner(System.in);
 		String start;
 		String player;
+		int set;
 		char[] [] BOARD = {{' ','|',' ','|',' '},
 				   {'-','+','-','+','-'},
 		                   {' ','|',' ','|',' '},
@@ -23,14 +24,18 @@ public class TicTacToe {
 		}while(!start.equals("Y"));
 			
 		theBOARD(BOARD);
-		
+	
 		do {
 		System.out.println("What you choose ? X/O");
 		
 		player = scanner.nextLine().toUpperCase();
 			
-		}while(!(player.equals("X") || player.equals("O"))); //Law of De Morgan!
-		}
+		}while(!(player.equals("X") || player.equals("O")));
+		System.out.println("Position ? 1-9");
+		set = scanner.nextInt();
+		place(BOARD,player,set);	
+		theBOARD(BOARD);
+	}
 	
 	public static void theBOARD(char[][] BOARD) {
 		for (char[] row : BOARD) {
@@ -40,6 +45,13 @@ public class TicTacToe {
 				System.out.println();
 				}
 		}
-	
+	public static void place(char[] [] BOARD, String player, int set) {
+		switch(set) {
+		case 1:
+		BOARD[0][0] = 'X';
+		break;
+		}
+		
+	}
 }
 
